be4ef69650fbfba6b17e693c793aa135f43f8ea3
Guy <guydwilks@gmail.com>
2020-11-27 01:16:11 -0800

    Wrote the entire frontend
    Spend about 4 hours on straight on this

--- Encryption/src/app/main-page/main-page.component.ts
+++ Encryption/src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-
+  public CCreturnArea = '';
+  CCinput: string;
+  VCreturnArea: any;
+  VCinput: any;
+  VCShiftinput: any;
+  publicKeyReturn: any;
+  privateKeyReturn: any;
+  PubKeyEncrpMessage: any;
+  encryptedMessage: any;
+  decryptedMessage: string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  encript_caesar(): void {
+    this.CCreturnArea = this.CCinput;
+  }
+
+  encript_Vingnere(): void {
+    const input = this.VCinput;
+    const shift = this.VCShiftinput;
+    this.VCreturnArea = shift;
+  }
+
+  generate_keys(): void {
+    this.privateKeyReturn = 'dsa';
+    this.publicKeyReturn = 'asd';
+  }
+
+  encript_message_public(): void {
+    const publicKey = this.publicKeyReturn;
+    const input = this.PubKeyEncrpMessage;
+    this.encryptedMessage = '';
+  }
+
+  decrypt_message_public(): void {
+    const privateKey = this.privateKeyReturn;
+    const encryptedMessage = this.encryptedMessage;
+    this.decryptedMessage = '';
+
+  }
 }
