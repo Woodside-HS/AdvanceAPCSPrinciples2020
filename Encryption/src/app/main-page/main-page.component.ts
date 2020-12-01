a9768d125b6d2513bf04b75fd1add72fba7695f7
807951 <54371368+807951@users.noreply.github.com>
2020-12-01 08:21:53 -0800

    Added caesar cypher

--- Encryption/src/app/main-page/main-page.component.ts
+++ Encryption/src/app/main-page/main-page.component.ts
@@ -23,6 +23,19 @@ export class MainPageComponent implements OnInit {
 
   encript_caesar(): void {
     this.CCreturnArea = this.CCinput;
+	for (let i = 0; i < this.CCinput.length; i++) {
+		let c = str[i];
+		if (c.match(/[a-z]/i)) {
+		  let code = str.charCodeAt(i);
+		  if (code >= 65 && code <= 90) {
+			c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+		  }
+		  else if (code >= 97 && code <= 122) {
+			c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+		  }
+		}
+		CCreturnArea += c;
+	}
   }
 
   encript_Vingnere(): void {
